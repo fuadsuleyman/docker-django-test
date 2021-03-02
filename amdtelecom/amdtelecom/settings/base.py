7a89614c93f5f4d19812596e09533ce8e84cb212
fuad_suleymanov <fuadsuleymanov@outlook.com>
2021-03-02 22:48:31 +0400

    ready to production

--- amdtelecom/amdtelecom/settings/base.py
+++ amdtelecom/amdtelecom/settings/base.py
@@ -142,10 +142,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+# deploy replace with below code
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
--- amdtelecom/amdtelecom/settings/dev.py
+++ amdtelecom/amdtelecom/settings/dev.py
@@ -1,11 +1,14 @@
+from amdtelecom.settings.base import BASE_DIR
 from amdtelecom.settings.base import *
+
+DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = '#p_$i#w56)lc@6a0)nz6&#%)3d8+yy62+-xy9zxa#6on-e!a5&1234567'
 SECRET_KEY = '#p_$i#w56)lc@6a0)nz6&#%)3d8+yy62+-xy9zxa#6on-e!a5&'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
-
-DEBUG = True
-
-# ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost', '127.0.0.1:8000']
